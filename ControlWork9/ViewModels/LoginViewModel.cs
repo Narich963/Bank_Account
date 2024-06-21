7b4e61b7a0f818e70539e4d79fc9042b09860dc3
Nariman Biiazbekov <narbiiaz@gmail.com>
2024-06-22 01:32:19 +0600

    Зараннее создал макет сайта с настроенным identity

---
+++ ControlWork9/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControlWork9.ViewModels;
+
+public class LoginViewModel
+{
+    [Required(ErrorMessage = "Login field is empty")]
+    public string Login { get; set; }
+
+    [Required(ErrorMessage = "Password field is empty")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+}
