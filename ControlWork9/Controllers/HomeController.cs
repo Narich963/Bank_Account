42426c05a85412d4f346be0b4f4336f70235bb52
Nariman Biiazbekov <narbiiaz@gmail.com>
2024-06-22 12:40:28 +0600

    Добавлена функция перевода денег с одного баланса на другой

--- ControlWork9/Controllers/HomeController.cs
+++ ControlWork9/Controllers/HomeController.cs
@@ -23,15 +23,49 @@ namespace ControlWork9.Controllers
         [HttpPost]
         public async Task<IActionResult> AddToBalance(int sum, int uniqueNumber)
         {
+            if (sum <= 0)
+            {
+                TempData["Error"] = "Недопустимое значение пополнения";
+                return RedirectToAction("Index");
+            }
             User user = await _context.Users.FirstOrDefaultAsync(u => u.UniqueNumber ==  uniqueNumber);
             if (user != null)
             {
                 user.Balance += sum;
                 _context.Update(user);
                 await _context.SaveChangesAsync();
+                TempData["Success"] = "Баланс успешно пополнен";
                 return RedirectToAction("Index");
             }
-            return NotFound();
+            TempData["Error"] = "Данный номер не был найден";
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Transfer(int sum, int uniqueNumber)
+        {
+            if (sum <= 0)
+            {
+                TempData["Error"] = "Недопустимое значение пополнения";
+                return RedirectToAction("Index");
+            }
+            User fromUser = await _userManager.GetUserAsync(User);
+            User toUser = await _context.Users.FirstOrDefaultAsync(u => u.UniqueNumber == uniqueNumber);
+            if (fromUser != null && toUser != null)
+            {
+                if (fromUser.Balance < sum)
+                {
+                    TempData["Error"] = "На балансе не достаточно средств";
+                    return RedirectToAction("Index");
+                }
+                fromUser.Balance -= sum;
+                toUser.Balance += sum;
+                _context.UpdateRange(fromUser, toUser);
+                await _context.SaveChangesAsync();
+                TempData["Success"] = "Деньги успешно переведены";
+                return RedirectToAction("Index");
+            }
+            TempData["Error"] = "Данный номер не был найден";
+            return RedirectToAction("Index");
         }
         public IActionResult Privacy()
         {
